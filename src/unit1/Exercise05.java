f87bd247efc034cdb9d2a7ba8b8d21148d821673
gonza <gonza@gonzalo>
2025-09-22 18:07:51 +0200

    a

--- src/unit1/Exercise05.java
+++ src/unit1/Exercise05.java
@@ -7,7 +7,21 @@ public class Exercise05 {
 	public static void main(String[] args) {
 		//Create a scanner
 		Scanner sc = new Scanner(System.in);
+		
+		//Create a radio variable
+		double radio;
+		
+		//Create a longitud variable
+		double longitud;
 				
+		//The console waits for us to enter a radio
+		radio = sc.nextInt();
+		
+		//Longitud is calculated
+		longitud = (3.14 * 2) + (radio);
+
+		//The console prints longitud
+		System.out.println("Longitud is:" + longitud);
 		
 		//Close the scanner
 		sc.close();
