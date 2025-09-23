0cca4527985f72e45e26cfc831b480cd19ffc3f4
gonzalo.lopez <gonzalo.lopez@106-14.info2>
2025-09-23 13:51:02 +0200

    a

---
+++ src/unit1/exercise09.java
@@ -0,0 +1,31 @@
+package unit1;
+
+import java.util.Scanner;
+
+public class exercise09 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Crea un scanner
+		Scanner sc = new Scanner(System.in);
+				
+		//Crea la variable
+		int edad;
+				
+		//La consola imprime que indiques tu edad
+		System.out.println("Introduce tu edad:");
+					
+		//La consola espera que indiques tu edad
+		edad = sc.nextInt();
+		
+		
+				
+		//La consola imprime
+		System.out.println(");
+				
+		//Close the scanner
+		sc.close();
+	}
+
+}
