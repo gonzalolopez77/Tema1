d110b7deb32d0df663dce651ee6b6453d9071c02
gonzalo.lopez <gonzalo.lopez@106-14.info2>
2025-09-25 14:41:45 +0200

    a

---
+++ src/unit1/exercise11.java
@@ -0,0 +1,35 @@
+package unit1;
+
+import java.util.Scanner;
+
+public class exercise11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Crea un scanner
+		Scanner sc = new Scanner(System.in);
+						
+		//Crea la variable pesetas
+		int pesetas;
+				
+		//Crea la variable euros		
+		double euros;
+						
+		//La consola imprime que indiques un numero de pesetas
+		System.out.println("Introduce un numero de pesetas:");
+							
+		//La consola espera que indiques un numero de pesetas
+		pesetas = sc.nextInt();
+		
+		//
+		euros = 
+						
+		//La consola imprime si es par
+		System.out.println("Es par  " + par);
+						
+		//Close the scanner
+		sc.close();		
+	}
+
+}
