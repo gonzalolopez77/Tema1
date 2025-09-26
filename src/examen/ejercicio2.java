f873073165cedf95fd8d4a6d2916951dc86115e5
gonzalo.lopez <gonzalo.lopez@106-14.info2>
2025-09-26 13:33:39 +0200

    a

--- src/examen/ejercicio2.java
+++ src/examen/ejercicio2.java
@@ -10,11 +10,20 @@ public class ejercicio2 {
 		//Crea un scanner
 		Scanner sc = new Scanner(System.in);
 		
+		//Crea la variable porcentaje1
+		final int porcentaje1 = 10;	
+		
+		//Crea la variable porcentaje2
+		final int porcentaje2 = 15;			
+		
 		//Crea la variable horas
 		int horas;	
 		
 		//Crea la variable sueldo
-		double sueldo;			
+		double sueldo;	
+		
+		//Crea la variable sueldoDescontado
+		double sueldoDescontado;
 		
 		//La consola imprime que indiques cuantas horas trabajas
 		System.out.println("Horas: ");
@@ -28,12 +37,14 @@ public class ejercicio2 {
 		//La consola espera que indiques tu sueldo por hora
 		sueldo = sc.nextDouble();			
 		
-		//Calculamos el precio con el IVA
-		precioIVA = (IVA * precio) / 100;
-		precioIVA += precio; 
+		//Calculamos tu sueldo semanal 
+		sueldo = (sueldo) * (horas);
 		
-		//La consola imprime el precio con IVA
-		System.out.println("Precio con IVA: " + precioIVA);
+		//Calculamos tu sueldo con impuestos
+		 sueldoDescontado = sueldo<1000 ? (sueldo * porcentaje1) /100 + sueldo: (sueldo * porcentaje2) /100 + sueldo ;
+		
+		//La consola imprime el sueldo con impuestos
+		System.out.println("Sueldo con impuestos: " + sueldoDescontado);
 				
 		//Close the scanner
 		sc.close();		
